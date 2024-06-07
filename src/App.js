f8f21cd959fb6a6da7d55dde0ae94ca937a59195
Vyvy <meannavy74@gmail.com>
2024-06-07 17:00:56 +0700

    msg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
-
+import { useState } from "react";
+import Child from "./child"
 function App() {
+
+  const [name] = useState("John");
+  console.log(name);
+
+
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Hello</h1>
+      <Child />
+    </>
   );
 }
 
